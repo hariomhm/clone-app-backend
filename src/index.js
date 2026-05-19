9364e823a0f7fbc7b0ecaeefd09de8efc2a1c82a
Hariom Mahawar <hariomhm111.com>
2026-05-19 14:23:43 +0530

    database connection and custom error handeling

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
-import dotenv from "dotenv"
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants.js";
 import connectDB from "./db/index.js";
-dotenv.config({
-    path: './.env'
-})
+import app from "./app.js";
+import dotenv from "dotenv";
 
-
+dotenv.config(
+    {
+        path: "./.env"
+    }
+);
 
 connectDB()
